0c4921b966760bbf956188e1477f8fe77ab0cf79
sana <benissia.fs@gmail.com>
2025-02-27 11:55:15 +0100

    v1

---
+++ mon-app-typescript/src/App.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import Greeting from './Greeting'; 
+import Counter from './Counter';
+
+function App() {
+  return (
+    <div>
+      <Greeting name="World" />
+      <Counter />
+    </div>
+  );
+}
+
+export default App;
